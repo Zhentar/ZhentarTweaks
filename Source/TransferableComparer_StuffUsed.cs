26d4e85c8f5844aee65aa0f13c32a224b638c514
Zhentar <Zhentar@gmail.com>
2017-01-26 09:18:37 -0600

    Add new trade/caravan load list sorters

---
+++ Source/TransferableComparer_StuffUsed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace ZhentarTweaks
+{
+	class TransferableComparer_StuffUsed : TransferableComparer
+	{
+		public override int Compare(ITransferable lhs, ITransferable rhs)
+		{
+			return GetValueFor(lhs).CompareTo(GetValueFor(rhs));
+		}
+
+		private string GetValueFor(ITransferable t)
+		{
+			Thing anyThing = t.AnyThing;
+
+			return anyThing.Stuff?.LabelAsStuff ?? "";
+		}
+	}
+}
---
+++ Source/TransferableComparer_ValuePerWeight.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace ZhentarTweaks
+{
+	class TransferableComparer_ValuePerWeight : TransferableComparer
+	{
+		public override int Compare(ITransferable lhs, ITransferable rhs)
+		{
+			return GetValueFor(lhs).CompareTo(GetValueFor(rhs));
+		}
+
+		private float GetValueFor(ITransferable t)
+		{
+			Thing anyThing = t.AnyThing;
+			if (!anyThing.def.useHitPoints)
+			{
+				return 1f;
+			}
+			return anyThing.MarketValue / anyThing.GetStatValue(StatDefOf.Mass);
+		}
+	}
+}
