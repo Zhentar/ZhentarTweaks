e449aedc02aad0cc1b7314db05e5281ae7c85381
Zhentar <Zhentar@gmail.com>
2017-02-24 09:44:47 -0600

    Convert MarketValueFilter detours to Harmony patches

--- Source/MarketValueFilter.cs
+++ Source/MarketValueFilter.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Harmony;
 using RimWorld;
 using UnityEngine;
 using Verse;
 
 namespace ZhentarTweaks
 {
-	class MarketValueFilter : Verse.ThingFilter
+	
+	class MarketValueFilter : ThingFilter
 	{
+		private static readonly Func<ThingFilter, Action> settingsChangedCallbackGet = Utils.GetFieldAccessor<ThingFilter, Action>("settingsChangedCallback");
+		private static readonly FieldInfo settingsChangedCallbackInfo = typeof(ThingFilter).GetField("settingsChangedCallback", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
+		private Action settingsChangedCallback { get { return settingsChangedCallbackGet(this); } set { settingsChangedCallbackInfo.SetValue(this, value); } }
+
 		private FloatRange allowedMarketValue = new FloatRange(0, float.MaxValue);
 
 		public FloatRange AllowedMarketValue
@@ -18,176 +24,46 @@ namespace ZhentarTweaks
 			set { allowedMarketValue = value; }
 		} 
 
-		public MarketValueFilter() : base()
-		{ }
+		public MarketValueFilter() { }
 
-		public MarketValueFilter(Action callback) : base(callback)
-		{ }
+		public MarketValueFilter(Action callback) : base(callback) { }
 
-		[DetourMember] //It's not virtual so I can't just implement an override
-		public void ExposeData()
+		public MarketValueFilter(ThingFilter oldFilter)
 		{
-			var localSpecFilters = disallowedSpecialFilters;
-			Scribe_Collections.LookList(ref localSpecFilters, "disallowedSpecialFilters", LookMode.Def);
-			disallowedSpecialFilters = localSpecFilters;
-			var aDefs = allowedDefs;
-			Scribe_Collections.LookHashSet(ref aDefs, "allowedDefs");
-			allowedDefs = aDefs;
-			var allowedHP = AllowedHitPointsPercents;
-			Scribe_Values.LookValue(ref allowedHP, "allowedHitPointsPercents");
-			AllowedHitPointsPercents = allowedHP;
-			var allowedQualities = AllowedQualityLevels;
-			Scribe_Values.LookValue(ref allowedQualities, "allowedQualityLevels");
-			AllowedQualityLevels = allowedQualities;
-			if (this.GetType() == typeof(MarketValueFilter))
+			this.settingsChangedCallback = settingsChangedCallbackGet(oldFilter);
+		}
+
+		[DetourMemberHarmonyPostfix] //It's not virtual so I can't just implement an override
+		public static void ExposeData(MarketValueFilter __instance)
+		{
+			if (__instance.GetType() == typeof(MarketValueFilter))
 			{
-				Scribe_Values.LookValue(ref allowedMarketValue, "allowedMarketValue");
+				Scribe_Values.LookValue(ref __instance.allowedMarketValue, "allowedMarketValue");
 			}
 		}
 
-		[DetourMember] //Also not virtual
-		public void CopyAllowancesFrom(MarketValueFilter other)
+		[DetourMemberHarmonyPostfix] //Also not virtual
+		public static void CopyAllowancesFrom(MarketValueFilter __instance, ThingFilter other)
 		{
-			allowedDefs.Clear();
-			foreach (ThingDef current in AllStorableThingDefs)
-			{
-				SetAllow(current, other.Allows(current));
-			}
-			disallowedSpecialFilters = other.disallowedSpecialFilters.ListFullCopyOrNull();
-			allowedHitPointsPercents = other.allowedHitPointsPercents;
-			allowedHitPointsConfigurable = other.allowedHitPointsConfigurable;
-			allowedQualities = other.allowedQualities;
-			allowedQualitiesConfigurable = other.allowedQualitiesConfigurable;
-			thingDefs = other.thingDefs.ListFullCopyOrNull();
-			categories = other.categories.ListFullCopyOrNull();
-			exceptedThingDefs = other.exceptedThingDefs.ListFullCopyOrNull();
-			exceptedCategories = other.exceptedCategories.ListFullCopyOrNull();
-			specialFiltersToAllow = other.specialFiltersToAllow.ListFullCopyOrNull();
-			specialFiltersToDisallow = other.specialFiltersToDisallow.ListFullCopyOrNull();
-			stuffCategoriesToAllow = other.stuffCategoriesToAllow.ListFullCopyOrNull();
-			allowAllWhoCanMake = other.allowAllWhoCanMake.ListFullCopyOrNull();
-			settingsChangedCallback?.Invoke();
-			
 			//Gotta make sure we aren't reading or writing unallocated memory
-			if (other.GetType() == this.GetType() && this.GetType() == typeof(MarketValueFilter))
+			if (other.GetType() == __instance.GetType() && __instance.GetType() == typeof(MarketValueFilter))
 			{
-				allowedMarketValue = other.allowedMarketValue;
+				__instance.allowedMarketValue = ((MarketValueFilter)other).allowedMarketValue;
 			}
 		}
 
-		[DetourMember] //Yup, not virtual again
-		public bool Allows(Thing t)
+		[DetourMemberHarmonyPostfix] //Yup, not virtual again
+		public static void Allows(MarketValueFilter __instance, ref bool __result, Thing t)
 		{
-			if (!Allows(t.def))
+			if (__instance.GetType() == typeof(MarketValueFilter))
 			{
-				return false;
-			}
-			if (t.def.useHitPoints)
-			{
-				float num = t.HitPoints / (float)t.MaxHitPoints;
-				num = Mathf.Round(num * 100f) / 100f;
-				if (!allowedHitPointsPercents.IncludesEpsilon(Mathf.Clamp01(num)))
+				if (!__instance.allowedMarketValue.IncludesEpsilon(t.GetInnerIfMinified().MarketValue))
 				{
-					return false;
-				}
-			}
-			if (allowedQualities != QualityRange.All && t.def.FollowQualityThingFilter())
-			{
-				QualityCategory p;
-				if (!t.TryGetQuality(out p))
-				{
-					p = QualityCategory.Normal;
-				}
-				if (!allowedQualities.Includes(p))
-				{
-					return false;
-				}
-			}
-			for (int i = 0; i < disallowedSpecialFilters.Count; i++)
-			{
-				if (disallowedSpecialFilters[i].Worker.Matches(t))
-				{
-					return false;
+					__result = false;
 				}
 			}
 
-			if (this.GetType() == typeof(MarketValueFilter))
-			{
-				if (!allowedMarketValue.IncludesEpsilon(t.GetInnerIfMinified().MarketValue))
-				{
-					return false;
-				}
-			}
-
-			return true;
+			__result = true;
 		}
-
-		private static IEnumerable<ThingDef> AllStorableThingDefs
-		{
-			get
-			{
-				return from def in DefDatabase<ThingDef>.AllDefs
-					   where def.EverStoreable
-					   select def;
-			}
-		}
-
-
-		private static readonly Func<ThingFilter, List<SpecialThingFilterDef>> disallowedSpecialFiltersGet = Utils.GetFieldAccessor<ThingFilter, List<SpecialThingFilterDef>>("disallowedSpecialFilters");
-		private static readonly FieldInfo disallowedSpecialFiltersInfo = typeof(ThingFilter).GetField("disallowedSpecialFilters", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private List<SpecialThingFilterDef> disallowedSpecialFilters { get { return disallowedSpecialFiltersGet(this); } set { disallowedSpecialFiltersInfo.SetValue(this, value); } }
-
-
-		private static readonly Func<ThingFilter, HashSet<ThingDef>> allowedDefsGet = Utils.GetFieldAccessor<ThingFilter, HashSet<ThingDef>>("allowedDefs");
-		private static readonly FieldInfo allowedDefsInfo = typeof(ThingFilter).GetField("allowedDefs", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private HashSet<ThingDef> allowedDefs { get { return allowedDefsGet(this); } set { allowedDefsInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, Action> settingsChangedCallbackGet = Utils.GetFieldAccessor<ThingFilter, Action>("settingsChangedCallback");
-		private static readonly FieldInfo settingsChangedCallbackInfo = typeof(ThingFilter).GetField("settingsChangedCallback", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private Action settingsChangedCallback { get { return settingsChangedCallbackGet(this); } set { settingsChangedCallbackInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, TreeNode_ThingCategory> displayRootCategoryIntGet = Utils.GetFieldAccessor<ThingFilter, TreeNode_ThingCategory>("displayRootCategoryInt");
-		private static readonly FieldInfo displayRootCategoryIntInfo = typeof(ThingFilter).GetField("displayRootCategoryInt", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private TreeNode_ThingCategory displayRootCategoryInt { get { return displayRootCategoryIntGet(this); } set { displayRootCategoryIntInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, FloatRange> allowedHitPointsPercentsGet = Utils.GetFieldAccessor<ThingFilter, FloatRange>("allowedHitPointsPercents");
-		private static readonly FieldInfo allowedHitPointsPercentsInfo = typeof(ThingFilter).GetField("allowedHitPointsPercents", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private FloatRange allowedHitPointsPercents { get { return allowedHitPointsPercentsGet(this); } set { allowedHitPointsPercentsInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, QualityRange> allowedQualitiesGet = Utils.GetFieldAccessor<ThingFilter, QualityRange>("allowedQualities");
-		private static readonly FieldInfo allowedQualitiesInfo = typeof(ThingFilter).GetField("allowedQualities", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private QualityRange allowedQualities { get { return allowedQualitiesGet(this); } set { allowedQualitiesInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, List<ThingDef>> thingDefsGet = Utils.GetFieldAccessor<ThingFilter, List<ThingDef>>("thingDefs");
-		private static readonly FieldInfo thingDefsInfo = typeof(ThingFilter).GetField("thingDefs", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private List<ThingDef> thingDefs { get { return thingDefsGet(this); } set { thingDefsInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, List<String>> categoriesGet = Utils.GetFieldAccessor<ThingFilter, List<String>>("categories");
-		private static readonly FieldInfo categoriesInfo = typeof(ThingFilter).GetField("categories", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private List<String> categories { get { return categoriesGet(this); } set { categoriesInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, List<ThingDef>> exceptedThingDefsGet = Utils.GetFieldAccessor<ThingFilter, List<ThingDef>>("exceptedThingDefs");
-		private static readonly FieldInfo exceptedThingDefsInfo = typeof(ThingFilter).GetField("exceptedThingDefs", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private List<ThingDef> exceptedThingDefs { get { return exceptedThingDefsGet(this); } set { exceptedThingDefsInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, List<String>> exceptedCategoriesGet = Utils.GetFieldAccessor<ThingFilter, List<String>>("exceptedCategories");
-		private static readonly FieldInfo exceptedCategoriesInfo = typeof(ThingFilter).GetField("exceptedCategories", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private List<String> exceptedCategories { get { return exceptedCategoriesGet(this); } set { exceptedCategoriesInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, List<String>> specialFiltersToAllowGet = Utils.GetFieldAccessor<ThingFilter, List<String>>("specialFiltersToAllow");
-		private static readonly FieldInfo specialFiltersToAllowInfo = typeof(ThingFilter).GetField("specialFiltersToAllow", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private List<String> specialFiltersToAllow { get { return specialFiltersToAllowGet(this); } set { specialFiltersToAllowInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, List<String>> specialFiltersToDisallowGet = Utils.GetFieldAccessor<ThingFilter, List<String>>("specialFiltersToDisallow");
-		private static readonly FieldInfo specialFiltersToDisallowInfo = typeof(ThingFilter).GetField("specialFiltersToDisallow", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private List<String> specialFiltersToDisallow { get { return specialFiltersToDisallowGet(this); } set { specialFiltersToDisallowInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, List<StuffCategoryDef>> stuffCategoriesToAllowGet = Utils.GetFieldAccessor<ThingFilter, List<StuffCategoryDef>>("stuffCategoriesToAllow");
-		private static readonly FieldInfo stuffCategoriesToAllowInfo = typeof(ThingFilter).GetField("stuffCategoriesToAllow", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private List<StuffCategoryDef> stuffCategoriesToAllow { get { return stuffCategoriesToAllowGet(this); } set { stuffCategoriesToAllowInfo.SetValue(this, value); } }
-
-		private static readonly Func<ThingFilter, List<ThingDef>> allowAllWhoCanMakeGet = Utils.GetFieldAccessor<ThingFilter, List<ThingDef>>("allowAllWhoCanMake");
-		private static readonly FieldInfo allowAllWhoCanMakeInfo = typeof(ThingFilter).GetField("allowAllWhoCanMake", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
-		private List<ThingDef> allowAllWhoCanMake { get { return allowAllWhoCanMakeGet(this); } set { allowAllWhoCanMakeInfo.SetValue(this, value); } }
 	}
 }
--- Source/MarketValueFilterInjector.cs
+++ Source/MarketValueFilterInjector.cs
@@ -9,224 +9,101 @@ using Object = System.Object;
 
 namespace ZhentarTweaks
 {
-	[StaticConstructorOnStartup]
-	class MarketValueFilterInjector
+	static class MarketValueFilterInjector
 	{
-		class Bill : Object
+		abstract class BillDetour : Bill
 		{
-			private static readonly Func<object, int> loadIDGet = Utils.GetFieldAccessorNoInherit<RimWorld.Bill, int>("loadID");
-			private static readonly FieldInfo loadIDInfo = typeof(RimWorld.Bill).GetField("loadID", Detours.UniversalBindingFlags);
-			private int loadID { get { return loadIDGet(this); } set { loadIDInfo.SetValue(this, value); } }
-			private static readonly Func<object, Single> ingredientSearchRadiusGet = Utils.GetFieldAccessorNoInherit<RimWorld.Bill, Single>("ingredientSearchRadius");
-			private static readonly FieldInfo ingredientSearchRadiusInfo = typeof(RimWorld.Bill).GetField("ingredientSearchRadius", Detours.UniversalBindingFlags);
-			private Single ingredientSearchRadius { get { return ingredientSearchRadiusGet(this); } set { ingredientSearchRadiusInfo.SetValue(this, value); } }
-			private static readonly Func<object, IntRange> allowedSkillRangeGet = Utils.GetFieldAccessorNoInherit<RimWorld.Bill, IntRange>("allowedSkillRange");
-			private static readonly FieldInfo allowedSkillRangeInfo = typeof(RimWorld.Bill).GetField("allowedSkillRange", Detours.UniversalBindingFlags);
-			private IntRange allowedSkillRange { get { return allowedSkillRangeGet(this); } set { allowedSkillRangeInfo.SetValue(this, value); } }
-			private static readonly Func<object, int> lastIngredientSearchFailTicksGet = Utils.GetFieldAccessorNoInherit<RimWorld.Bill, int>("lastIngredientSearchFailTicks");
-			private static readonly FieldInfo lastIngredientSearchFailTicksInfo = typeof(RimWorld.Bill).GetField("lastIngredientSearchFailTicks", Detours.UniversalBindingFlags);
-			private int lastIngredientSearchFailTicks { get { return lastIngredientSearchFailTicksGet(this); } set { lastIngredientSearchFailTicksInfo.SetValue(this, value); } }
-			private static readonly Func<object, Verse.RecipeDef> recipeGet = Utils.GetFieldAccessorNoInherit<RimWorld.Bill, Verse.RecipeDef>("recipe");
-			private static readonly FieldInfo recipeInfo = typeof(RimWorld.Bill).GetField("recipe", Detours.UniversalBindingFlags);
-			private Verse.RecipeDef recipe { get { return recipeGet(this); } set { recipeInfo.SetValue(this, value); } }
-			private static readonly Func<object, ThingFilter> ingredientFilterGet = Utils.GetFieldAccessorNoInherit<RimWorld.Bill, ThingFilter>("ingredientFilter");
-			private static readonly FieldInfo ingredientFilterInfo = typeof(RimWorld.Bill).GetField("ingredientFilter", Detours.UniversalBindingFlags);
-			private ThingFilter ingredientFilter { get { return ingredientFilterGet(this); } set { ingredientFilterInfo.SetValue(this, value); } }
-			private static readonly Func<object, bool> suspendedGet = Utils.GetFieldAccessorNoInherit<RimWorld.Bill, bool>("suspended");
-			private static readonly FieldInfo suspendedInfo = typeof(RimWorld.Bill).GetField("suspended", Detours.UniversalBindingFlags);
-			private bool suspended { get { return suspendedGet(this); } set { suspendedInfo.SetValue(this, value); } }
-
-			[DetourConstructor(typeof(RimWorld.Bill))]
-			public Bill(Verse.RecipeDef recipe)
+			[DetourConstructorHarmonyPostfix(typeof(RimWorld.Bill))]
+			public static void Bill(Bill __instance, Verse.RecipeDef recipe)
 			{
-				this.loadID = -1;
-				this.ingredientSearchRadius = 999f;
-				this.allowedSkillRange = new IntRange(0, 20);
-				this.lastIngredientSearchFailTicks = -99999;
-				this.recipe = recipe;
-				this.ingredientFilter = new MarketValueFilter();
-				this.ingredientFilter.CopyAllowancesFrom(recipe.defaultIngredientFilter);
-				this.loadID = Find.World.uniqueIDsManager.GetNextBillID();
+				__instance.ingredientFilter = new MarketValueFilter();
+				__instance.ingredientFilter.CopyAllowancesFrom(recipe.defaultIngredientFilter);
 			}
 
-			[DetourMember(typeof(RimWorld.Bill))]
-			public virtual void ExposeData()
+			[DetourMemberHarmonyPostfix]
+			public static void ExposeData(Bill __instance)
 			{
-				var loadIDtmp = loadID;
-				Scribe_Values.LookValue(ref loadIDtmp, "loadID");
-				loadID = loadIDtmp;
-				var recipetmp = recipe;
-				Scribe_Defs.LookDef(ref recipetmp, "recipe");
-				recipe = recipetmp;
-				var suspendedtmp = suspended;
-				Scribe_Values.LookValue(ref suspendedtmp, "suspended");
-				suspended = suspendedtmp;
-				var ingredientSearchRadiustmp = ingredientSearchRadius;
-				Scribe_Values.LookValue(ref ingredientSearchRadiustmp, "ingredientSearchRadius", 999f);
-				ingredientSearchRadius = ingredientSearchRadiustmp;
-				var allowedSkillRangetmp = allowedSkillRange;
-				Scribe_Values.LookValue(ref allowedSkillRangetmp, "allowedSkillRange");
-				allowedSkillRange = allowedSkillRangetmp;
-				if (Scribe.mode == LoadSaveMode.Saving && recipe.fixedIngredientFilter != null)
-				{
-					foreach (ThingDef current in DefDatabase<ThingDef>.AllDefs)
-					{
-						if (!recipe.fixedIngredientFilter.Allows(current))
-						{
-							ingredientFilter.SetAllow(current, false);
-						}
-					}
-				}
-				var ingredientFiltertmp = ingredientFilter;
-				Scribe_Deep.LookDeep(ref ingredientFiltertmp, "ingredientFilter");
-				ingredientFilter = ingredientFiltertmp;
+				var ingredientFiltertmp = __instance.ingredientFilter;
 				if (Scribe.mode == LoadSaveMode.LoadingVars && ingredientFiltertmp.GetType() != typeof(MarketValueFilter))
 				{
-					ingredientFilter = new MarketValueFilter();
-					if (ingredientFiltertmp != null) { ingredientFilter.CopyAllowancesFrom(ingredientFiltertmp); }
+					__instance.ingredientFilter = new MarketValueFilter();
+					if (ingredientFiltertmp != null) { __instance.ingredientFilter.CopyAllowancesFrom(ingredientFiltertmp); }
 				}
 			}
 		}
 
 		class OutfitDetour : Outfit
 		{
-			[DetourConstructor(typeof(Outfit))]
-			public void Outfit()
+			[DetourConstructorHarmonyPostfix(typeof(Outfit))]
+			public static void Outfit(Outfit __instance)
 			{
-				this.filter = new MarketValueFilter();
-			}
-			[DetourConstructor(typeof(Outfit))]
-			public void Outfit(int uniqueId, string label)
-			{
-				this.filter = new MarketValueFilter();
-				this.uniqueId = uniqueId;
-				this.label = label;
+				__instance.filter = new MarketValueFilter();
 			}
 
-			[DetourMember]
-			public void ExposeData()
+			[DetourMemberHarmonyPostfix]
+			public static void ExposeData(Outfit __instance)
 			{
-				Scribe_Values.LookValue(ref uniqueId, "uniqueId");
-				Scribe_Values.LookValue(ref label, "label");
-				Scribe_Deep.LookDeep(ref filter, "filter");
-				if (Scribe.mode == LoadSaveMode.LoadingVars && filter.GetType() != typeof(MarketValueFilter))
+				if (Scribe.mode == LoadSaveMode.LoadingVars && __instance.filter.GetType() != typeof(MarketValueFilter))
 				{
-					var tempFilter = filter;
-					filter = new MarketValueFilter();
-					filter.CopyAllowancesFrom(tempFilter);
+					var tempFilter = __instance.filter;
+					__instance.filter = new MarketValueFilter();
+					__instance.filter.CopyAllowancesFrom(tempFilter);
 				}
 			}
 		}
 
 		class StorageSettingsDetour : StorageSettings
 		{
-			private static readonly Func<object, StoragePriority> priorityIntGet = Utils.GetFieldAccessorNoInherit<StorageSettings, StoragePriority>("priorityInt");
-			private static readonly FieldInfo priorityIntInfo = typeof(StorageSettings).GetField("priorityInt", Detours.UniversalBindingFlags);
-			private StoragePriority priorityInt { get { return priorityIntGet(this); } set { priorityIntInfo.SetValue(this, value); } }
-
 			private static readonly Action<StorageSettings> NotifyChanged = Utils.GetMethodInvoker<StorageSettings>("TryNotifyChanged");
 
-			[DetourConstructor(typeof(StorageSettings))]
-			public void StorageSettings()
+
+			[DetourConstructorHarmonyPostfix(typeof(StorageSettings))]
+			public static void StorageSettings(StorageSettings __instance)
 			{
-				this.priorityInt = StoragePriority.Normal;
-				this.filter = new MarketValueFilter( () => NotifyChanged(this));
+				__instance.filter = new MarketValueFilter(__instance.filter);
 			}
 
-
-			[DetourConstructor(typeof(RimWorld.StorageSettings))]
-			public void StorageSettings(IStoreSettingsParent owner)
+			[DetourConstructorHarmonyPostfix(typeof(StorageSettings))]
+			public static void StorageSettings(StorageSettings __instance, IStoreSettingsParent owner)
 			{
-				this.priorityInt = StoragePriority.Normal;
-				this.owner = owner;
-				if (owner != null)
-				{
-					StorageSettings parentStoreSettings = owner.GetParentStoreSettings();
-					if (parentStoreSettings != null)
-					{
-						this.priorityInt = priorityIntGet(parentStoreSettings);
-					}
-				}
-				this.filter = new MarketValueFilter(() => NotifyChanged(this));
+				__instance.filter = new MarketValueFilter(__instance.filter);
 			}
 
-
-			[DetourMember]
-			public void ExposeData()
+			[DetourMemberHarmonyPostfix]
+			public static void ExposeData(StorageSettings __instance)
 			{
-				var priorityInttmp = priorityInt;
-				Scribe_Values.LookValue(ref priorityInttmp, "priority");
-				priorityInt = priorityInttmp;
-				Scribe_Deep.LookDeep(ref filter, "filter");
-				if (Scribe.mode == LoadSaveMode.LoadingVars && filter.GetType() != typeof(MarketValueFilter))
+				if (Scribe.mode == LoadSaveMode.LoadingVars && __instance.filter.GetType() != typeof(MarketValueFilter))
 				{
-					var tempFilter = filter;
-					filter = new MarketValueFilter();
-					if (tempFilter != null) { filter.CopyAllowancesFrom(tempFilter); }
+					var tempFilter = __instance.filter;
+					__instance.filter = new MarketValueFilter();
+					if (tempFilter != null) { __instance.filter.CopyAllowancesFrom(tempFilter); }
 				}
 			}
 		}
 
-		class Dialog_ManageOutfitsDetour : Dialog_ManageOutfits
+		class Dialog_ManageOutfitsDetour
 		{
-			public Dialog_ManageOutfitsDetour(Outfit selectedOutfit) : base(selectedOutfit)
-			{}
-
-			[DetourConstructor(typeof(Dialog_ManageOutfits))]
-			public void Dialog_ManageOutfits(Outfit selectedOutfit)
+			[DetourConstructorHarmonyPostfix(typeof(Dialog_ManageOutfits))]
+			public static void Dialog_ManageOutfits(Dialog_ManageOutfits __instance, Outfit selectedOutfit)
 			{
-				this.forcePause = true;
-				this.doCloseX = true;
-				this.closeOnEscapeKey = true;
-				this.doCloseButton = true;
-				this.closeOnClickedOutside = true;
-				this.absorbInputAroundWindow = true;
 				if (apparelGlobalFilterGet() == null)
 				{
 					var filter = new MarketValueFilter();
 					apparelGlobalFilterInfo.SetValue(null, filter);
 					filter.SetAllow(ThingCategoryDefOf.Apparel, true);
 				}
-				SelectedOutfitInfo.SetValue(this, selectedOutfit, null);
-
-				//Window constructor, since this doesn't call base..ctor
-				this.layer = WindowLayer.Dialog;
-				this.closeOnEscapeKey = true;
-				this.preventCameraMotion = true;
-				this.doWindowBackground = true;
-				this.onlyOneOfTypeAllowed = true;
-				this.drawShadow = true;
-				this.focusWhenOpened = true;
-				this.shadowAlpha = 1f;
-				CloseButSizeInfo.SetValue(this, new Vector2(120f, 40f)); //readonly field
-				this.soundAppear = SoundDefOf.DialogBoxAppear;
-				this.soundClose = SoundDefOf.Click;
 			}
 
-			private static readonly FieldInfo CloseButSizeInfo = typeof(Dialog_ManageOutfits).GetField("CloseButSize", Detours.UniversalBindingFlags);
-			private static readonly PropertyInfo SelectedOutfitInfo = typeof(Dialog_ManageOutfits).GetProperty("SelectedOutfit", Detours.UniversalBindingFlags);
 			private static readonly Func<ThingFilter> apparelGlobalFilterGet = Utils.GetStaticFieldAccessor<Dialog_ManageOutfits, ThingFilter>("apparelGlobalFilter");
 			private static readonly FieldInfo apparelGlobalFilterInfo = typeof(Dialog_ManageOutfits).GetField("apparelGlobalFilter", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.GetField);
-
-			public override void DoWindowContents(Rect inRect)
-			{
-				throw new NotImplementedException();
-			}
 		}
 
-		class IngredientCount : Object
+		class IngredientCountDetour
 		{
-			private static readonly Func<object, ThingFilter> filterGet = Utils.GetFieldAccessorNoInherit<Verse.IngredientCount, ThingFilter>("filter");
-			private static readonly FieldInfo filterInfo = typeof(Verse.IngredientCount).GetField("filter", Detours.UniversalBindingFlags);
-			private ThingFilter filter { get { return filterGet(this); } set { filterInfo.SetValue(this, value); } }
-			private static readonly Func<object, Single> countGet = Utils.GetFieldAccessorNoInherit<Verse.IngredientCount, Single>("count");
-			private static readonly FieldInfo countInfo = typeof(Verse.IngredientCount).GetField("count", Detours.UniversalBindingFlags);
-			private Single count { get { return countGet(this); } set { countInfo.SetValue(this, value); } }
-			[DetourConstructor(typeof(Verse.IngredientCount))]
-			public IngredientCount()
+			[DetourConstructorHarmonyPostfix(typeof(Verse.IngredientCount))]
+			public static void IngredientCount(IngredientCount __instance)
 			{
-				this.filter = new MarketValueFilter();
-				this.count = 1f;
+				__instance.filter = new MarketValueFilter();
 			}
 		}
 
